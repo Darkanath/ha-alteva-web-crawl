ad80b50ab9c558ba1c08bb74aed63874950ba4a1
Tal <tal.shterzer@gmail.com>
2026-09-15 19:52:56 +0300

    fix: run EF migrations on startup

--- src/Alteva.CrawlApi/Program.cs
+++ src/Alteva.CrawlApi/Program.cs
@@ -44,6 +44,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // ==========================================
 // HTTP Request Pipeline
 // ==========================================
